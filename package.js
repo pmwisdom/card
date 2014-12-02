7006688db7b1d41690c6c17913e02026bf9edd24
Paul Wisdom <paul@flybuy.com>
2014-12-02 09:55:57 -0800

    Add card files

---
+++ package.js
@@ -0,0 +1,35 @@
+Package.describe({
+  name: 'mirrorcell:card',
+  summary: ' A meteor warpper for Card by jessepollak  ',
+  version: '1.0.0',
+  git: ' /* Fill me in! */ '
+});
+
+Npm.depends({'jquery.payment' : "1.1.3"});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.use(['jquery'])
+ 
+  api.addFiles([
+  	'card/lib/css/card.css', 
+  	'card/lib/js/card.js',
+  	'card/src/coffee/card.coffee',
+  	'card/src/scss/card.scss', 
+  	'card/src/scss/_mixins.scss',
+  	'card/src/scss/cards/_amex.scss',
+  	'card/src/scss/cards/_card.scss',
+  	'card/src/scss/cards/_discover.scss',
+  	'card/src/scss/cards/_mastercard.scss',
+  	'card/src/scss/cards/_visa.scss'], 
+
+    'client');
+
+  api.export('card');
+});
+//Tests
+Package.onTest(function(api) {
+  api.use('tinytest');
+  //api.use('mirrorcell:card');
+  api.addFiles('mirrorcell:card-tests.js');
+});
