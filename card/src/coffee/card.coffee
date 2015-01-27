701cc63d8adaa2a53356eb8ef84b5a0d30c6d88d
Paul Wisdom <paul@flybuy.com>
2015-01-27 09:40:24 -0800

    fix missing coffeescript reference, removed require from card.coffee and moved it to the package

--- card/src/coffee/card.coffee
+++ card/src/coffee/card.coffee
@@ -1,6 +1,4 @@
-﻿require 'jquery.payment'
-
-$ = jQuery
+﻿$ = jQuery
 $.card = {}
 $.card.fn = {}
 $.fn.card = (opts) ->
--- package.js
+++ package.js
@@ -1,35 +1,37 @@
 Package.describe({
-  name: 'mirrorcell:card',
-  summary: ' A meteor warpper for Card by jessepollak  ',
-  version: '1.0.0',
-  git: ' /* Fill me in! */ '
+    name: 'mirrorcell:card',
+    summary: ' A meteor wrapper for Card by jessepollak  ',
+    version: '1.0.1',
+    git: '',
+    documentation: 'README.md'
 });
 
 Npm.depends({'jquery.payment' : "1.1.3"});
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use(['jquery'])
+    api.versionsFrom('1.0.3.1');
+
+    api.use(['jquery', 'coffeescript']);
+    api.imply(['jquery', 'coffeescript']);
  
-  api.addFiles([
-  	'card/lib/css/card.css', 
-  	'card/lib/js/card.js',
-  	'card/src/coffee/card.coffee',
-  	'card/src/scss/card.scss', 
-  	'card/src/scss/_mixins.scss',
-  	'card/src/scss/cards/_amex.scss',
-  	'card/src/scss/cards/_card.scss',
-  	'card/src/scss/cards/_discover.scss',
-  	'card/src/scss/cards/_mastercard.scss',
-  	'card/src/scss/cards/_visa.scss'], 
+    api.addFiles([
+  	    'card/lib/css/card.css',
+  	    'card/lib/js/card.js',
+  	    'card/src/coffee/card.coffee',
+  	    'card/src/scss/card.scss',
+  	    'card/src/scss/_mixins.scss',
+  	    'card/src/scss/cards/_amex.scss',
+  	    'card/src/scss/cards/_card.scss',
+  	    'card/src/scss/cards/_discover.scss',
+  	    'card/src/scss/cards/_mastercard.scss',
+  	    'card/src/scss/cards/_visa.scss'],
+        'client');
 
-    'client');
-
-  api.export('card');
+    api.export('Card');
 });
 //Tests
 Package.onTest(function(api) {
-  api.use('tinytest');
-  //api.use('mirrorcell:card');
-  api.addFiles('mirrorcell:card-tests.js');
+    api.use('tinytest');
+    //api.use('mirrorcell:card');
+    api.addFiles('mirrorcell:card-tests.js');
 });
